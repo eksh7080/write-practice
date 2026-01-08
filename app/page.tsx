e91a06ac9899a334950b4f8e289d641760b59c81
leedano <eksh7080@gmil.com>
2026-01-09 00:22:32 +0900

    [home-develop] feat: 단편소설 텍스트 파일 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,14 @@
+/**
+ * 설명: 루트 페이지
+ * **/
 import styles from '@/scss/module/home.module.scss';
-import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
       <section className={styles.mainContainer}>
         <div className={styles.mainWrap}>
-          <div className={styles.mainGridContainer}>
-            <Link href="">
-              <article>⌨️</article>
-              <strong>자리 연습</strong>
-            </Link>
-            <Link href="">
-              <article>📝</article>
-              <strong>낱말 연습</strong>
-            </Link>
-            <Link href="">
-              <article>✏️</article>
-              <strong>단문 연습</strong>
-            </Link>
-            <Link href="">
-              <article>📄</article>
-              <strong>장문 연습</strong>
-            </Link>
-          </div>
+          <div className={styles.mainGridContainer}></div>
         </div>
       </section>
     </main>
